03d45f661a233364051e071bfd074718d9bc1a77
Illia <karma@MYPC>
2023-10-08 10:50:41 +0200

    Add project files.

---
+++ App2(Illia Karmazin)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2_Illia_Karmazin_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
